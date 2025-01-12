b4d09a77015491e15eb3242d0bb9676df2122221
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-01-13 01:34:30 +0300

    create TelegramLogin component

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,22 +1,14 @@
 import { FaBitcoin } from 'react-icons/fa6';
-import { LoginButton } from '@telegram-auth/react';
+import { useQuery } from '@tanstack/react-query';
 import { useAppKitAccount } from '@reown/appkit/react';
-import { useMutation, useQuery } from '@tanstack/react-query';
 
-import {
-  addPoints,
-  getRating,
-  getBuiedFiles,
-  verifiedWithTelegram,
-  getVerifiedWithTelegram,
-} from '../utils/http';
 import { useToast } from '@/hooks/use-toast';
-import PurchasedList from '@/components/PurchasedList';
 import SkeletonList from '@/components/SkeletonList';
+import PurchasedList from '@/components/PurchasedList';
+import TelegramLogin from '@/components/TelegramLogin';
+import { getRating, getBuiedFiles } from '../utils/http';
 
 const Portfolio = () => {
-  let content;
-
   const { toast } = useToast();
   const { address } = useAppKitAccount();
 
@@ -42,53 +34,10 @@ const Portfolio = () => {
     enabled: !!address,
   });
 
-  const { data: verfiedWithTele, isLoading: isVerfiedWithTele } = useQuery({
-    queryKey: ['isVerfiedWithTele', { address }],
-    queryFn: () => getVerifiedWithTelegram({ address: address! }),
-    enabled: !!address,
-  });
-
-  const { mutate } = useMutation({
-    mutationFn: verifiedWithTelegram,
-    onSuccess: ({ success, message }) => {
-      if (success) {
-        addPointsMutation({ address: address!, points: '500' });
-      } else content = message;
-    },
-  });
-
-  const { mutate: addPointsMutation } = useMutation({
-    mutationFn: addPoints,
-    onSuccess: () => {
-      content = 'Thanks, you will get 500 points';
-    },
-  });
-
   return (
     <>
       <section className="bg-[#6B39F4] h-[284px] flex justify-center items-center *:text-white">
-        <div className="text-center">
-          {isVerfiedWithTele && <p>Loading...</p>}
-
-          {verfiedWithTele && !verfiedWithTele.isverified && (
-            <div className="flex flex-col justify-center items-center gap-5">
-              <p>Verified Your account with telegram to get 500 point</p>
-
-              <LoginButton
-                lang="en"
-                cornerRadius={5}
-                showAvatar={true}
-                buttonSize="large"
-                botUsername={import.meta.env.VITE_BOT_USER_NAME}
-                onAuthCallback={({ id }) => {
-                  if (id) mutate({ address: address!, userId: id });
-                }}
-              />
-
-              {content}
-            </div>
-          )}
-        </div>
+        <TelegramLogin />
       </section>
 
       <section className="w-[90%] mx-auto mt-10 mb-5">
